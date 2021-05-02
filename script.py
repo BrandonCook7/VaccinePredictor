dad675105c5ab58c2d7e300b5413eb86275cab6c
Brandon Cook <54552002+BrandonCook7@users.noreply.github.com>
2021-05-01 17:22:26 -0700

    Tools to view Data and Functions to calculate percentages

--- script.py
+++ script.py
@@ -6,6 +6,7 @@ from datetime import date
 
 unordered_vacc_df = pd.read_csv("datasets/us_state_vaccinations.csv")
 state_vacc = [pd.DataFrame]
+state_vacc_dict = {}
 us_state_pop = pd.read_csv("datasets/2019_Census_Data_Population.csv")
 #print(unordered_vacc_df)
 vacc_df = unordered_vacc_df[['date','location','total_vaccinations','total_distributed','people_vaccinated','people_fully_vaccinated', 'daily_vaccinations']].copy()
@@ -18,17 +19,17 @@ def removeNonStates():
     stateList = []
     for i in range(len(stateNames)):
         stateList.append(stateNames[i])
-    print(stateList)
+    #print(stateList)
     vacc_df_temp = vacc_df
     temp_location = vacc_df_temp['location']
     i = len(temp_location)-1
-    print(vacc_df)
+    #print(vacc_df)
     
     while i > 0:
         try:
             index = stateList.index(temp_location[i])
         except ValueError:
-            print(vacc_df['location'][i])
+            #print(vacc_df['location'][i])
 
             vacc_df.drop(vacc_df.index[i], inplace=True,axis=0)#Inplace keeps the orginal without needing a copy, axis=0 means to go by rows
         i -= 1
@@ -49,19 +50,44 @@ def orderByStates():#This splits up the main dataframe into smaller dataframes n
         if (vacc_df['location'][i] != currentLoc) and (i != 0):
             del tempDf
             tempDf = pd.DataFrame()
-            tempDf = vacc_df.loc[startOfLoc:i]
+            tempDf = vacc_df.loc[startOfLoc:i-1]
             startOfLoc = i
             
             state_vacc.append(tempDf)
+            state_vacc_dict.update({vacc_df['location'][i-1] : tempDf})
         currentLoc = vacc_df['location'][i]
             
+def getPercVacc(date, state):#Given a data and state it returns the percent of people vaccinated in that state on that date
+    state_data = state_vacc_dict.get(state)#Gets state Dataframe
+    date_data = state_data.loc[state_data['date'] == date]#Gets row of date from dataframe
+    vacc_on_date = date_data.iloc[0]['people_fully_vaccinated']#Gets value of vaccinated from row
+
+    state_data = us_state_pop[us_state_pop['State'] == state]
+    pop_of_state = state_data.iloc[0]['Population_Estimate']
+    percVaccinated = vacc_on_date/pop_of_state
 
 
-#print(us_state_pop)
-#print(state_vacc)
-print('hello')
-#print(current_date)
+    #print(vacc_on_date)
+    #print(pop_of_state)
+    #print(percVaccinated)
+    return percVaccinated
+
+def graphVaccinated(dateUntil, state):
+    start_date = date(2021, 1, 12)
+    end_date = dateUntil
+    date_range = pd.date_range(start_date, end_date)
+    vaccList = []
+    dateList = []
+    for index_date in date_range:
+        vaccPerc = getPercVacc(index_date.strftime("%Y-%m-%d"), state)
+        vaccList.append(vaccPerc)
+        dateList.append(index_date.strftime("%Y-%m-%d"))
+    plt.plot(vaccList)
+    plt.show()
+
+
 removeNonStates()
 orderByStates()
 
-print(state_vacc)
+graphVaccinated(date(2021, 4, 20), "New Hampshire")
+
